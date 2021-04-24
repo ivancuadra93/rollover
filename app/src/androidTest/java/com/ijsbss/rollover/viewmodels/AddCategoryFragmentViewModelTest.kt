93ed015c4b5234d2b1f60352b61d3b15531492c8
Jorge Escobar <jorge.escobar0419@gmail.com>
2021-04-23 23:50:36 -0700

    test: add view-model tests for AddCategory and Main

--- app/src/androidTest/java/com/ijsbss/rollover/viewmodels/AddCategoryFragmentViewModelTest.kt
+++ app/src/androidTest/java/com/ijsbss/rollover/viewmodels/AddCategoryFragmentViewModelTest.kt
@@ -11,6 +11,7 @@ import com.ijsbss.rollover.data.db.CategoryRepository
 import com.ijsbss.rollover.runBlockingTest
 import com.ijsbss.rollover.utilities.*
 import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.lessThanOrEqualTo
 import org.junit.*
 import org.junit.Assert.assertThat
 import org.junit.rules.RuleChain
@@ -61,7 +62,7 @@ class AddCategoryFragmentViewModelTest {
         assertThat(viewModel.inputColor.value, equalTo("Red"))
         assertThat(viewModel.inputThreshold.value, equalTo("90"))
 
-        // save as input field values as a category in db
+        // save input field values as a category in db
         viewModel.saveOrUpdate()
 
         // check for new category added to db
@@ -72,13 +73,91 @@ class AddCategoryFragmentViewModelTest {
         assertThat(vmCategories[0].rolloverPeriod, equalTo(14))
         assertThat(vmCategories[0].color, equalTo(RED))
         assertThat(vmCategories[0].threshold, equalTo(90.0F))
+
+        // remove from db
+        repository.delete(vmCategories[0].id)
     }
 
-    // test threshold over 100% - should fail
+    @Test
+    fun testNameCapitalization() = coroutineRule.runBlockingTest {
+        // add values to bindable data to simulate input
+        viewModel.inputName.value = "food"
+        viewModel.inputExpectation.value = "200.00"
+        viewModel.inputRolloverPeriod.value = "45"
+        viewModel.inputColor.value = "Red"
+        viewModel.inputThreshold.value = "90"
 
-    // test rollover period limits - not more than a month?
+        // save input field values as a category in db
+        viewModel.saveOrUpdate()
 
-    // test delete for category
+        // check Name was Capitalized for storage
+        val vmCategories = repository.categories.getOrAwaitValue()
+        assert(vmCategories[0].name != "food")
 
-    // test name capitalization for storage
+        // remove from db
+        repository.delete(vmCategories[0].id)
+    }
+
+    @Test
+    fun testDuplicateCategoryNames() = coroutineRule.runBlockingTest {
+        @Suppress("UNUSED_PARAMETER")
+        for (i in 1..2) {
+            // add values to bindable data to simulate input
+            viewModel.inputName.value = "food"
+            viewModel.inputExpectation.value = "200.00"
+            viewModel.inputRolloverPeriod.value = "45"
+            viewModel.inputColor.value = "Red"
+            viewModel.inputThreshold.value = "90"
+
+            // save input field values as a category in db
+            viewModel.saveOrUpdate()
+        }
+
+        // check Name was Capitalized for storage
+        val vmCategories = repository.categories.getOrAwaitValue()
+        assertThat(vmCategories.size, equalTo(1))
+
+        // remove from db
+        repository.delete(vmCategories[0].id)
+    }
+
+    @Test
+    fun testThresholdLimitOver100Percent() = coroutineRule.runBlockingTest {
+        // add values to bindable data to simulate input
+        viewModel.inputName.value = "food"
+        viewModel.inputExpectation.value = "200.00"
+        viewModel.inputRolloverPeriod.value = "14"
+        viewModel.inputColor.value = "Red"
+        viewModel.inputThreshold.value = "150.00"
+
+        // save input field values as a category in db
+        viewModel.saveOrUpdate()
+
+        // check that view model rejected input of greater than 100.00 for threshold
+        val vmCategories = repository.categories.getOrAwaitValue()
+        assertThat(vmCategories[0].threshold, lessThanOrEqualTo(100.00F))
+
+        // remove from db
+        repository.delete(vmCategories[0].id)
+    }
+
+    fun testRolloverPeriodLimitOver28Days() = coroutineRule.runBlockingTest {
+        // add values to bindable data to simulate input
+        viewModel.inputName.value = "food"
+        viewModel.inputExpectation.value = "200.00"
+        viewModel.inputRolloverPeriod.value = "45"
+        viewModel.inputColor.value = "Red"
+        viewModel.inputThreshold.value = "90"
+
+
+        // save input field values as a category in db
+        viewModel.saveOrUpdate()
+
+        // check that view model rejected input of greater than 28 for rollover period
+        val vmCategories = repository.categories.getOrAwaitValue()
+        assertThat(vmCategories[0].rolloverPeriod, lessThanOrEqualTo(28))
+
+        // remove from db
+        repository.delete(vmCategories[0].id)
+    }
 }
