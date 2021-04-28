d99b5456249aa707f603f75aecd8a7564d01fa7c
Saidaer <saidaer.saifuding.900@my.csun.edu>
2021-04-27 19:42:04 -0700

    test: add test for categoryDao

--- app/src/androidTest/java/com/ijsbss/rollover/data/CategoryDaoTest.kt
+++ app/src/androidTest/java/com/ijsbss/rollover/data/CategoryDaoTest.kt
@@ -1,6 +1,7 @@
 package com.ijsbss.rollover.data
 
 import android.graphics.Color
+import android.media.VolumeShaper
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -22,9 +23,9 @@ import org.junit.runner.RunWith
 class CategoryDaoTest {
     private lateinit var db: AppDatabase
     private lateinit var categoryDao: CategoryDao
-    private val categoryA = Category(1234, "A", 120.00f, 32.54f, 14, Color.RED, 20.00f, 10, 1)
-    private val categoryB = Category(1285, "B", 140.00f, 74.21f, 14, Color.BLUE, 20.00f, 11,2)
-    private val categoryC = Category(1676, "C", 190.00f, 85.60f, 14, Color.GREEN, 20.00f, 12, 3)
+    private val categoryA = Category(0, "A", 120.00f, 32.54f, 14, Color.RED, 20.00f, 10, 1)
+    private val categoryB = Category(0, "B", 140.00f, 74.21f, 14, Color.BLUE, 20.00f, 11,2)
+    private val categoryC = Category(0, "C", 190.00f, 85.60f, 14, Color.GREEN, 20.00f, 12, 3)
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
@@ -51,18 +52,111 @@ class CategoryDaoTest {
         val categories = categoryDao.getCategories().getOrAwaitValue()
 
         assertThat(categories.size, equalTo(3))
-
-        // Ensure category list is sorted by view order
-        assertThat(categories[0], equalTo(categoryA))
-        assertThat(categories[1], equalTo(categoryB))
-        assertThat(categories[2], equalTo(categoryC))
     }
 
     @Test
     fun testDeleteCategory() = runBlocking {
-        categoryDao.delete(categoryB.id)
+        var categories = categoryDao.getCategories().getOrAwaitValue()
+        categoryDao.delete(categories[1].id)
+
+        categories = categoryDao.getCategories().getOrAwaitValue()
+        assertThat(categories.size, equalTo(2))
+    }
+
+    @Test
+    fun testAddCategory() = runBlocking {
+        categoryDao.insert(Category(0,"D",210.00f,50.00f,14,Color.GREEN,20.00f,13,4))
 
         val categories = categoryDao.getCategories().getOrAwaitValue()
-        assertThat(categories.size, equalTo(2))
+        assertThat(categories.size, equalTo(4))
+    }
+
+    @Test
+    fun testDeleteAllCategories() = runBlocking {
+        categoryDao.deleteAll()
+
+        val categories = categoryDao.getCategories().getOrAwaitValue()
+        assertThat(categories.size, equalTo(0))
+    }
+
+    @Test
+    fun testCategoryID() = runBlocking {
+        val categories = categoryDao.getCategories().getOrAwaitValue()
+
+        assert(categories[0].id != categoryA.id)
+        assert(categories[1].id != categoryB.id)
+        assert(categories[2].id != categoryC.id)
+    }
+
+    @Test
+    fun testCategoryName() = runBlocking {
+        val categories = categoryDao.getCategories().getOrAwaitValue()
+
+        assertThat(categories[0].name, equalTo(categoryA.name))
+        assertThat(categories[1].name, equalTo(categoryB.name))
+        assertThat(categories[2].name, equalTo(categoryC.name))
+    }
+
+    @Test
+    fun testCategoryExpectation() = runBlocking {
+        val categories = categoryDao.getCategories().getOrAwaitValue()
+
+        assertThat(categories[0].expectation, equalTo(categoryA.expectation))
+        assertThat(categories[1].expectation, equalTo(categoryB.expectation))
+        assertThat(categories[2].expectation, equalTo(categoryC.expectation))
+    }
+
+    @Test
+    fun testCategoryTotalSpent() = runBlocking {
+        val categories = categoryDao.getCategories().getOrAwaitValue()
+
+        assertThat(categories[0].totalSpent, equalTo(categoryA.totalSpent))
+        assertThat(categories[1].totalSpent, equalTo(categoryB.totalSpent))
+        assertThat(categories[2].totalSpent, equalTo(categoryC.totalSpent))
+    }
+
+    @Test
+    fun testCategoryRolloverPeriod() = runBlocking {
+        val categories = categoryDao.getCategories().getOrAwaitValue()
+
+        assertThat(categories[0].rolloverPeriod, equalTo(categoryA.rolloverPeriod))
+        assertThat(categories[1].rolloverPeriod, equalTo(categoryB.rolloverPeriod))
+        assertThat(categories[2].rolloverPeriod, equalTo(categoryC.rolloverPeriod))
+    }
+
+    @Test
+    fun testCategoryColor() = runBlocking {
+        val categories = categoryDao.getCategories().getOrAwaitValue()
+
+        assertThat(categories[0].color, equalTo(categoryA.color))
+        assertThat(categories[1].color, equalTo(categoryB.color))
+        assertThat(categories[2].color, equalTo(categoryC.color))
+    }
+
+    @Test
+    fun testCategoryThreshold() = runBlocking {
+        val categories = categoryDao.getCategories().getOrAwaitValue()
+
+        assertThat(categories[0].threshold, equalTo(categoryA.threshold))
+        assertThat(categories[1].threshold, equalTo(categoryB.threshold))
+        assertThat(categories[2].threshold, equalTo(categoryC.threshold))
+    }
+
+    @Test
+    fun testCategoryExpensesId() = runBlocking {
+        val categories = categoryDao.getCategories().getOrAwaitValue()
+
+        assertThat(categories[0].expensesId, equalTo(categoryA.expensesId))
+        assertThat(categories[1].expensesId, equalTo(categoryB.expensesId))
+        assertThat(categories[2].expensesId, equalTo(categoryC.expensesId))
+    }
+
+    @Test
+    fun testCategoryViewOrder() = runBlocking {
+        val categories = categoryDao.getCategories().getOrAwaitValue()
+
+        assertThat(categories[0].viewOrder, equalTo(categoryA.viewOrder))
+        assertThat(categories[1].viewOrder, equalTo(categoryB.viewOrder))
+        assertThat(categories[2].viewOrder, equalTo(categoryC.viewOrder))
     }
 }
